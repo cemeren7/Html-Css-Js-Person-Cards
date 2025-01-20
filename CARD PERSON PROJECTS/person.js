75b8e3fc902b9fdcf46140a3e8fe2f2597d76360
cem eren yardımcı <er7tr1245@gmail.com>
2025-01-20 18:04:02 +0300

    Html-Css-Javascript-Person-Blog
    Html Css Javascript  kullarak geliştirdiğim basit seviyede bir blog arayüzü

---
+++ CARD PERSON PROJECTS/person.js
@@ -0,0 +1,23 @@
+function read1(){
+    document.getElementById("read1").style.display="block";
+}
+
+function read1hide(){
+    document.getElementById("read1").style.display="none";
+}
+
+function read2(){
+    document.getElementById("read2").style.display="block";
+}
+
+function read2hide(){
+    document.getElementById("read2").style.display="none";
+}
+
+function read3(){
+    document.getElementById("read3").style.display="block";
+}
+
+function read3hide(){
+    document.getElementById("read3").style.display="none";
+}
